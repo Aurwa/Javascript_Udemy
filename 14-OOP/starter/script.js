f57802c9c319e7d0d95250707f4a82a28c6a9d62
Aurwa <zafaraurwa11@gmail.com>
2024-06-10 11:55:20 +0500

    Chaining Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -291,6 +291,7 @@ jessica.introduce();
 
 // ***** Inheritance btwn classess object.create ******
 
+/*
 const PersonProto = {
   calcAge() {
     console.log(2024 - this.birthYear);
@@ -312,3 +313,48 @@ StudentProto.init = function (firstName, birthYear, course) {
 };
 
 const jay = Object.create(StudentProto);
+*/
+
+// ****** Another Class Example ********
+
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this._movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account ${owner}`);
+  }
+
+  getMovements() {
+    return this._movements;
+  }
+
+  deposit(val) {
+    this._movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+
+  approveLoan(val) {
+    return true;
+  }
+
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+
+console.log(acc1);
