4f05cadfc41559bddc5f35d8a1d6a91b6ccecacb
Aurwa <zafaraurwa11@gmail.com>
2024-06-14 23:13:30 +0500

    Functions_Default_Parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const bookingArr = [];
+const createBooking = function flight(
+  flightNum,
+  numPassengers = 10,
+  price = 1000
+) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+  bookingArr.push(booking);
+};
+
+createBooking('LH123');
