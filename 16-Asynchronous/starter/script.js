a9524135adfc2dd7c7ff8d607306202ef333e713
Aurwa <zafaraurwa11@gmail.com>
2024-06-23 21:12:10 +0500

    Update script.js

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -22,4 +22,169 @@ function getCountryData(country) {
     });
 }
 
-getCountryData('canada');
+// getCountryData('canada');
+
+const promiseOne = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    // console.log('Async task is complete!');
+    resolve();
+  }, 2000);
+});
+
+promiseOne.then(() => {
+  // console.log('Promise consumed.');
+});
+
+// console.log('Hello World');
+
+// const promiseThree = new Promise(function (resolve, reject) {
+//   setTimeout(() => {
+//     let error = true;
+//     if (!error) {
+//       resolve({ username: 'Chai', city: 'Lahore' });
+//     } else {
+//       reject('ERROR');
+//     }
+//   }, 3000);
+// });
+
+// promiseThree
+//   .then(user => {
+//     return user.username;
+//   })
+//   .then(name => {
+//     // console.log(name);
+// });
+
+// const promiseFour = new Promise(function (reject, resolve) {
+//   setTimeout(function () {
+//     let error = true;
+//     if (!error) {
+//       resolve({ username: 'Javascript', password: '123' });
+//     } else {
+//       reject('ERROR!!!');
+//     }
+//   }, 2000);
+// });
+
+// async function consumePromiseFour() {
+//   try {
+//     const response = await promiseFour;
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// consumePromiseFour();
+
+// async function getAllUsers() {
+//   try {
+//     const response = await fetch('https://randomuser.me/api/');
+//     const data = await response.json();
+//     console.log(data);
+//     console.log(data.results[0].name);
+//   } catch (error) {
+//     console.log('Error ', error);
+//   }
+// }
+
+// getAllUsers();
+
+// console.log('Hello World');
+
+// const getUsers = new Promise(function (resolve, reject) {
+//   fetch('https://randomuser.me/api/')
+//     .then(function (response) {
+//       if (!response.ok) {
+//         throw new Error('Network response was not okay.');
+//       } else {
+//         return response.json();
+//       }
+//     })
+//     .then(function (data) {
+//       resolve(data);
+//     })
+//     .catch(function (error) {
+//       reject(error);
+//     });
+// });
+
+// getUsers
+//   .then(function (data) {
+//     console.log(data.results);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+// function users() {
+//   fetch('https://randomuser.me/api/')
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
+// }
+
+// users();
+
+//  fetch('https://randomuser.me/api/')
+
+//=======================================
+// creating promise
+// ======================================
+
+// const getUsers = new Promise(function (resolve, reject) {
+//   fetch('https://randomuser.me/api/')
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       resolve(data);
+//     })
+//     .catch(function (error) {
+//       reject('Errrrorrr!!!');
+//     });
+// });
+
+// getUsers
+//   .then(function (data) {
+//     console.log(data);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+
+//=======================================
+// consuming promise
+// ======================================
+
+// const users = function () {
+//   fetch('https://randomuser.me/api/')
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//     });
+// };
+
+// users();
+
+//=======================================
+// async await
+// ======================================
+
+async function getUsers() {
+  try {
+    const response = await fetch('https://randomuser.me/api/');
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getUsers();
