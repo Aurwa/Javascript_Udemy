d86a5ab3ad523d4f5469d57a733f0f4b0a9e5ab7
Aurwa <zafaraurwa11@gmail.com>
2024-06-26 20:31:53 +0500

    localStorage_js

--- Local_Storage/script.js
+++ Local_Storage/script.js
@@ -1,4 +1,5 @@
 let form = document.querySelector("form");
+let main = document.querySelector(".main");
 
 form.addEventListener("submit", (event) => {
   let name = event.target.uname.value;
@@ -15,6 +16,38 @@ form.addEventListener("submit", (event) => {
   });
 
   localStorage.setItem("userDetails", JSON.stringify(userData));
-  // console.log(userData);
+
+  displayData();
+  event.target.reset();
   event.preventDefault();
 });
+
+let displayData = () => {
+  let userData = JSON.parse(localStorage.getItem("userDetails")) ?? [];
+  let finalData = "";
+  userData.forEach((element, index) => {
+    finalData += `
+     <div class="items">
+        <span onClick='removeData(${index})'>&times</span>
+        <h5>Name</h5>
+        <div>${element.name}</div>
+
+        <h5>Email</h5>
+        <div>${element.email}</div>
+
+        <h5>Phone</h5>
+        <div>${element.phone}</div>
+      </div>
+   `;
+  });
+  main.innerHTML = finalData;
+};
+
+let removeData = (index) => {
+  let userData = JSON.parse(localStorage.getItem("userDetails")) ?? [];
+  userData.splice(index, 1);
+  localStorage.setItem("userDetails", JSON.stringify(userData));
+  displayData();
+};
+
+displayData();
