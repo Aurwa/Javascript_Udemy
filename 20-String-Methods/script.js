e1517127c6243a5323dc310db332de52cd81a189
Aurwa <zafaraurwa11@gmail.com>
2024-07-22 22:40:49 +0500

    Update script.js

--- 20-String-Methods/script.js
+++ 20-String-Methods/script.js
@@ -1,5 +1,11 @@
-let str = "Aurwa Zafarar";
-console.log(str.lastIndexOf("a", 8));
-// console.log(str.indexOf("r", 6));
+let str = "Aurwa Zafarr";
+// console.log(str.lastIndexOf("a", 8));
+// console.log(str.indexOf("a", 6));
 // console.log(str.indexOf("a", 5));
 // console.log(str.charAt(2));
+console.log(str.lastIndexOf("a", -6));
+
+let str2 = "the code undefined code code !";
+// console.log(str2.lastIndexOf("code", -10));
+// console.log(str2.indexOf("code", -15));
+// console.log(str2.length);
