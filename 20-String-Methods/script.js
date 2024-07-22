641443a6f5202831c43a064620b1f5d21daf54ee
Aurwa <zafaraurwa11@gmail.com>
2024-07-22 23:03:19 +0500

    Update script.js

--- 20-String-Methods/script.js
+++ 20-String-Methods/script.js
@@ -1,11 +1,28 @@
+// indexOf + lastIndexOf
+/*
 let str = "Aurwa Zafarr";
-// console.log(str.lastIndexOf("a", 8));
-// console.log(str.indexOf("a", 6));
-// console.log(str.indexOf("a", 5));
-// console.log(str.charAt(2));
+console.log(str.lastIndexOf("a", 8));
+console.log(str.indexOf("a", 6));
+console.log(str.indexOf("a", 5));
+console.log(str.charAt(2));
 console.log(str.lastIndexOf("a", -6));
 
 let str2 = "the code undefined code code !";
-// console.log(str2.lastIndexOf("code", -10));
-// console.log(str2.indexOf("code", -15));
-// console.log(str2.length);
+console.log(str2.lastIndexOf("code", -10));
+console.log(str2.indexOf("code", -15));
+console.log(str2.length);
+*/
+
+// slice + substring
+let str = "The morning is upon us";
+// console.log(str.slice(12));
+// console.log(str.substring(12));
+
+// console.log(str.slice(-11));
+// console.log(str.substring(-11));
+
+// console.log(str.slice(12, 16));
+// console.log(str.substring(12, 16));
+
+console.log(str.slice(16, 12));
+console.log(str.substring(10, 5));
