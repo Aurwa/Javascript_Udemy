6fdf3019179ef2360d5391a269cf5901468f8bce
Aurwa <zafaraurwa11@gmail.com>
2024-07-28 23:27:01 +0500

    map_method

--- 11-Arrays-Bankist/array_methods/script.js
+++ 11-Arrays-Bankist/array_methods/script.js
@@ -1,5 +1,5 @@
 // forEach
-
+/*
 let nums = [1, 2, 3, 4, 5];
 nums.forEach((item, index, arr) => {
   // console.log("num [ " + index + "] = " + item);
@@ -24,3 +24,48 @@ letters.forEach((letter, index, arr) => {
   }
 });
 // console.log(count);
+*/
+
+// map
+
+const nums = [1, 2, 3, 4, 5];
+let res = 1;
+nums.map((num) => {
+  return (res *= num);
+});
+// console.log(res);
+
+const numbers = [2, 4, 5];
+let doubleNum = numbers.map((num) => {
+  return num * 2;
+});
+// console.log(doubleNum);
+
+const products = [
+  {
+    name: "laptop",
+    price: 1000,
+    count: 5,
+  },
+  {
+    name: "desktop",
+    price: 5000,
+    count: 10,
+  },
+];
+
+let itemWorth = products.map((product) => {
+  return {
+    name: product.name,
+    total: product.price * product.count,
+  };
+});
+// console.log(itemWorth);
+
+let strNum = ["1", "2", "3"];
+let nums2 = strNum.map((num) => {
+  return Number(num);
+});
+
+// console.log(nums2);
+// console.log(typeof nums2[0]);
