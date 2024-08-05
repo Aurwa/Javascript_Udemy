3bac67917584eb5b4ba27e83d3c1bb0ca8f0cc52
Aurwa <zafaraurwa11@gmail.com>
2024-08-05 11:36:46 +0500

    loops

---
+++ JS - Part 4/script.js
@@ -0,0 +1,11 @@
+// printing all odd nums
+console.log("Odd");
+for (let i = 15; i >= 1; i -= 2) {
+  console.log(i);
+}
+
+// printing all even
+console.log("Even");
+for (let i = 0; i < 15; i += 2) {
+  console.log(i);
+}
