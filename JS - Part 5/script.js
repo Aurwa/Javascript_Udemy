59654ed5a8c2738d58176b80a65b8e073297f4f8
Aurwa <zafaraurwa11@gmail.com>
2024-08-08 01:28:34 +0500

    objects_js

---
+++ JS - Part 5/script.js
@@ -0,0 +1,17 @@
+let obj = {
+  1: "a",
+  2: "b",
+  age: 23,
+};
+
+const student = {
+  name: "Aurwa",
+  age: 26,
+  marks: 94.4,
+  city: "Delhi",
+};
+
+student.city = "Mumbai";
+student.gender = "female";
+student.marks = "A";
+console.log(student);
