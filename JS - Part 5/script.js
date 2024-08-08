6c33631c7537354c65d37d874dde368734e1b296
Aurwa <zafaraurwa11@gmail.com>
2024-08-08 16:14:28 +0500

    Update script.js

--- JS - Part 5/script.js
+++ JS - Part 5/script.js
@@ -14,4 +14,34 @@ const student = {
 student.city = "Mumbai";
 student.gender = "female";
 student.marks = "A";
-console.log(student);
+// console.log(student);
+
+// Generate random number
+/*
+let max = prompt("Enter a range:");
+
+let randomNum = Math.floor(Math.random() * max) + 1;
+
+let guess = prompt("Enter your guess");
+
+while (true) {
+  if (guess == "quit") {
+    console.log("User quits");
+    break;
+  }
+
+  if (guess == randomNum) {
+    console.log("You guessed it. The number was: ", randomNum);
+    break;
+  } else if (guess < randomNum) {
+    guess = prompt("Take a higher guess");
+  } else {
+    guess = prompt("Take a lower guess");
+  }
+}
+*/
+
+// let arr = [1, 2, 3, 4, 5, 6, 2, 3];
+
+let num = Math.floor(Math.random() * 6) + 1;
+console.log(num);
