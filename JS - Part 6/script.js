f89c735a96067b5e3c12d1e20269d2a667d1f3d4
Aurwa <zafaraurwa11@gmail.com>
2024-08-08 21:54:58 +0500

    Update script.js

--- JS - Part 6/script.js
+++ JS - Part 6/script.js
@@ -13,4 +13,37 @@ function table(n) {
   }
 }
 
-table(2);
+// table(2);
+
+let res = 0;
+function sum(n) {
+  for (let i = 1; i <= n; i++) {
+    res += i;
+  }
+  return res;
+}
+
+// console.log(sum(6));
+
+let str = ["apple", "banana"];
+function concat(arr) {
+  let res = "";
+  for (let i = 0; i < arr.length; i++) {
+    res += arr[i];
+  }
+  return res;
+}
+
+// console.log(concat(str));
+
+let greet = "hello";
+
+function changGreet() {
+  let greet = "namaste";
+  console.log(greet);
+  function innerGreet() {
+    console.log(greet);
+  }
+}
+console.log(greet);
+changGreet();
