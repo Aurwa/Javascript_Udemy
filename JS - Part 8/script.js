b9dd6f8a56b3b3aac5c36363a354f5026a075e71
Aurwa <zafaraurwa11@gmail.com>
2024-08-09 23:02:12 +0500

    Update script.js

--- JS - Part 8/script.js
+++ JS - Part 8/script.js
@@ -1,3 +1,4 @@
+/*
 let arr = [1, 2, 3, 4, 5, 6, 9, 2];
 
 let even = arr.filter((num) => {
@@ -25,3 +26,30 @@ let arr2 = [10, 20, 30, 40, 50];
 
 let ans2 = arr2.every((el) => el % 10 == 0);
 console.log(ans2);
+*/
+
+// function sum(a, b = 1) {
+//   return a + b;
+// }
+
+// let x = sum(2, 6);
+// console.log(x);
+
+// console.log(..."apnacollege");
+
+let arr = [1, 2, 3, 4, 5];
+console.log(...arr);
+
+let newArr = [...arr];
+console.log(newArr);
+
+let chars = [..."hello"];
+console.log(chars);
+
+let str = "hello";
+let newStr = str.split("");
+console.log(newStr);
+
+let data = {
+  email: "zafar@yahoo.com",
+};
