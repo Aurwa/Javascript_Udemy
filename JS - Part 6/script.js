3b6055319fad92bd42746da2c9102df36e8f19dc
Aurwa <zafaraurwa11@gmail.com>
2024-08-09 11:33:36 +0500

    Update script.js

--- JS - Part 6/script.js
+++ JS - Part 6/script.js
@@ -25,25 +25,61 @@ function sum(n) {
 
 // console.log(sum(6));
 
-let str = ["apple", "banana"];
-function concat(arr) {
-  let res = "";
-  for (let i = 0; i < arr.length; i++) {
-    res += arr[i];
-  }
-  return res;
-}
+// let str = ["apple", "banana"];
+// function concat(arr) {
+//   let res = "";
+//   for (let i = 0; i < arr.length; i++) {
+//     res += arr[i];
+//   }
+//   return res;
+// }
 
 // console.log(concat(str));
 
-let greet = "hello";
-
-function changGreet() {
-  let greet = "namaste";
-  console.log(greet);
-  function innerGreet() {
-    console.log(greet);
+function oddOrEven(request) {
+  if (request == "odd") {
+    return function (n) {
+      console.log(!(n % 2 == 0));
+    };
+  } else if (request == "even") {
+    return function (n) {
+      console.log(n % 2 == 0);
+    };
+  } else {
+    console.log("Wrong request");
   }
 }
-console.log(greet);
-changGreet();
+
+let func = oddOrEven("even");
+// console.log(func);
+
+// return array elements larger than a number
+
+// let arr = [5, 3, 2, 6, 7, 1, 9];
+// let newArr = [];
+
+// function largerNum(arr, num) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > num) {
+//       newArr.push(arr[i]);
+//     }
+//   }
+//   return newArr;
+// }
+
+// console.log(largerNum(arr, 3));
+
+// extract unique characters from a string
+// let str = "abcdabcdefgggh";
+// let newStr = "";
+
+// function uniqueChar(str) {
+//   for (let i = 0; i < str.length; i++) {
+//     if (str.indexOf(str[i]) === i) {
+//       newStr += str[i];
+//     }
+//   }
+//   return newStr;
+// }
+
+// console.log(uniqueChar(str));
