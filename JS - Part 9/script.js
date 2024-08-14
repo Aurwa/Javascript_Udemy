8753f3c712f230d4d1db359967f4cd794d72672e
Aurwa <zafaraurwa11@gmail.com>
2024-08-14 12:12:02 +0500

    Update script.js

--- JS - Part 9/script.js
+++ JS - Part 9/script.js
@@ -1,2 +1,12 @@
 let h = document.all[6];
 h.innerHTML = "Peter Parker";
+
+let paras = document.getElementsByClassName("descp");
+
+// console.log(document.getElementsByClassName("descp")[0]);
+
+for (let i = 0; i < paras.length; i++) {
+  // console.dir(paras[i]);
+  // console.dir(paras[i].src) - incase I have an image;
+  console.dir(paras[i].textContent);
+}
