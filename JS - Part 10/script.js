c16a8b5fbb13dbe1bb71cf5e5eaa9e8370622c60
Aurwa <zafaraurwa11@gmail.com>
2024-08-16 18:36:55 +0500

    Update script.js

--- JS - Part 10/script.js
+++ JS - Part 10/script.js
@@ -1,13 +1,21 @@
-let btn = document.querySelector("button");
-
-// function clicked() {
-//   console.log("Button was clicked");
-// }
+let btns = document.querySelectorAll("button");
 
 //btn.onclick = clicked; // clicked function is assigned as event handler
+
+// btn.onmouseenter = entered;
+
+for (btn of btns) {
+  // btn.onclick = entered;
+  // btn.onclick = clicked;
+
+  btn.addEventListener("click", entered);
+  btn.addEventListener("click", clicked);
+}
 
 function entered() {
   console.log("Event triggered.");
 }
 
-btn.onmouseenter = entered;
+function clicked() {
+  console.log("Button was clicked");
+}
