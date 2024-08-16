cf1069533b12f348c0c255cdd7016983bc8b86f4
Aurwa <zafaraurwa11@gmail.com>
2024-08-16 19:51:07 +0500

    events_js

--- JS - Part 10/script.js
+++ JS - Part 10/script.js
@@ -1,3 +1,4 @@
+/*
 let btns = document.querySelectorAll("button");
 
 //btn.onclick = clicked; // clicked function is assigned as event handler
@@ -18,4 +19,26 @@ function entered() {
 
 function clicked() {
   console.log("Button was clicked");
+}
+*/
+
+let btn = document.querySelector("button");
+let div = document.querySelector("div");
+let h3 = document.querySelector("h3");
+
+btn.addEventListener("click", function () {
+  let randomColor = generateColor();
+  h3.innerText = randomColor;
+  div.style.backgroundColor = randomColor;
+});
+
+function generateColor() {
+  let red = Math.floor(Math.random() * 255);
+
+  let green = Math.floor(Math.random() * 255);
+
+  let blue = Math.floor(Math.random() * 255);
+
+  let color = `rgb(${red},${green}, ${blue})`;
+  return color;
 }
