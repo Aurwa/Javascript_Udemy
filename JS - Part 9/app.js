2bb645cdfe501c2121b72b4a60c24106db41e0eb
Aurwa <zafaraurwa11@gmail.com>
2024-08-16 14:11:50 +0500

    Update app.js

--- JS - Part 9/app.js
+++ JS - Part 9/app.js
@@ -5,5 +5,15 @@ console.log(document.all[6]);
 let paras = document.getElementsByClassName("descp");
 
 for (let i = 0; i < paras.length; i++) {
-  console.log(paras[i].innerText);
+  // console.log(paras[i].innerText);
+}
+
+let links = document.querySelectorAll(".box  a");
+
+// for (link of links) {
+//   console.log(link.innerText);
+// }
+
+for (let i = 0; i < links.length; i++) {
+  console.log(links[i].innerText);
 }
