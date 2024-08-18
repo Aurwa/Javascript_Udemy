4eb550a25bb1edb2af142bc12a21f4fc7a5a4e50
Aurwa <zafaraurwa11@gmail.com>
2024-08-18 20:35:46 +0500

    events_js

--- JS - Part 10/script.js
+++ JS - Part 10/script.js
@@ -22,6 +22,7 @@ function clicked() {
 }
 */
 
+/*
 let btn = document.querySelector("button");
 let div = document.querySelector("div");
 let h3 = document.querySelector("h3");
@@ -41,4 +42,44 @@ function generateColor() {
 
   let color = `rgb(${red},${green}, ${blue})`;
   return color;
+}
+*/
+
+let btn = document.querySelector("button");
+let div = document.querySelector("div");
+let h3 = document.querySelector("h3");
+
+btn.addEventListener("click", function () {
+  let randomColor = changeColor();
+  div.style.backgroundColor = randomColor;
+  h3.innerText = randomColor;
+});
+
+function changeColor() {
+  let red = Math.floor(Math.random() * 255);
+  let green = Math.floor(Math.random() * 255);
+  let blue = Math.floor(Math.random() * 255);
+  let color = `rgb(${red},${green},${blue})`;
+  return color;
+}
+
+/*
+Event Handler Assignment (e.g., btn.onclick = clicked;):
+In the previous example, you assigned a function to handle an event. You want the function to be called later when the event occurs (e.g., when the button is clicked). Therefore, you use btn.onclick = clicked; without the parentheses, because you're only assigning the function itself, not calling it immediately.
+
+Function Call and Assignment (e.g., let randomColor = generateColor();):
+Here, you're calling the generateColor function to immediately execute it and retrieve the result. You need the result right away to use in your code (to update the text and background color). Hence, you use parentheses to invoke the function.
+*/
+
+let testBtn = document.querySelector("#btn");
+let p = document.querySelector("p");
+let h1 = document.querySelector("h1");
+let h2 = document.querySelector("h2");
+
+p.addEventListener("click", changeCol);
+testBtn.addEventListener("click", changeCol);
+
+function changeCol() {
+  console.log(this);
+  this.style.backgroundColor = "red";
 }
