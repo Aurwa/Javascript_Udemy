2f80e469d461f28b20ca2aa2a5c701cb5ef3b87e
Aurwa <zafaraurwa11@gmail.com>
2024-08-21 12:22:49 +0500

    Update script.js

--- JS - Part 11/simon_game/script.js
+++ JS - Part 11/simon_game/script.js
@@ -10,7 +10,6 @@ let h2 = document.querySelector("h2");
 document.addEventListener("keypress", function () {
   if (started == false) {
     started = true;
-
     levelUp();
   }
 });
@@ -35,6 +34,7 @@ function userFlash(btn) {
 
 // function levelUp
 function levelUp() {
+  userSeq = [];
   level++;
   h2.innerText = `Level ${level}`;
 
@@ -43,22 +43,49 @@ function levelUp() {
   let randColor = btns[randIdx];
   let randBtn = document.querySelector(`.${randColor}`);
   gameSeq.push(randColor);
-  console.log(gameSeq);
+  console.log("Game seq: ", gameSeq);
 
   gameFlash(randBtn);
+}
+
+// function checkAns (checkSeq)
+function checkAns(idx) {
+  // console.log("Current level: ", level);
+  // let idx = level - 1;
+  if (userSeq[idx] === gameSeq[idx]) {
+    if (userSeq.length == gameSeq.length) {
+      setTimeout(levelUp, 1000);
+    }
+  } else {
+    h2.innerHTML = `Game Over! Your score was <b>${level}</b>. <br>Press any key to start.</br>`;
+    document.querySelector("body").style.backgroundColor = "red";
+    setTimeout(function () {
+      document.querySelector("body").style.backgroundColor = "white";
+    }, 400);
+    reset();
+  }
 }
 
 // function btnPress
 function btnPress() {
   let btn = this;
-  userFlash(btn);
+  userFlash(btn); // whole button / div is being passed
 
   let userColor = btn.getAttribute("id");
   userSeq.push(userColor);
-  console.log(userSeq);
+  console.log("User seq: ", userSeq);
+  checkAns(userSeq.length - 1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
 for (btn of allBtns) {
   btn.addEventListener("click", btnPress);
+}
+
+// function reset()
+function reset() {
+  started = false;
+  gameSeq = [];
+  userSeq = [];
+  level = 0;
 }
