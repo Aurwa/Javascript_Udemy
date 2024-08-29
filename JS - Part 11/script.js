b8b6ade2775cf33e350b984f629472a87749d274
Aurwa <zafaraurwa11@gmail.com>
2024-08-29 06:50:47 +0500

    Update script.js

--- JS - Part 11/script.js
+++ JS - Part 11/script.js
@@ -12,3 +12,41 @@ changeColor("red", 1000, () => {
 });
 // changeColor("purple", 2000);
 // changeColor("green", 3000);
+
+// function saveData(data, success, failure) {
+//   let speed = Math.floor(Math.random() * 10) + 1;
+//   if (speed > 4) {
+//     success();
+//   } else {
+//     failure();
+//   }
+// }
+
+// saveData(
+//   "Apna college",
+//   () => {
+//     console.log("Data was saved.");
+//   },
+//   () => {
+//     console.log("Weak connection");
+//   }
+// );
+
+function saveData(data) {
+  return new Promise((success, failure) => {
+    let speed = Math.floor(Math.random() * 10) + 1;
+    if (speed > 4) {
+      success("data was saved");
+    } else {
+      failure("failure: weak connection");
+    }
+  });
+}
+
+saveData("apna College")
+  .then(() => {
+    console.log("Success Promise");
+  })
+  .catch(() => {
+    console.log("Fail Promise");
+  });
